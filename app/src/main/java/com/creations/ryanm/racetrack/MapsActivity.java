2b73cd876f085451d28c67b476a54d9f5b7369c4
Ryan Malencia <ryan.malencia@gmail.com>
2017-08-22 21:10:19 -0400

    Feature developments
    -Trace path on map
    -Start/Stop button for showing time
    -Improved Pause/Resume functionality

--- app/src/main/java/com/creations/ryanm/racetrack/MapsActivity.java
+++ app/src/main/java/com/creations/ryanm/racetrack/MapsActivity.java
@@ -3,6 +3,7 @@ package com.creations.ryanm.racetrack;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.LocationManager;
+import android.os.SystemClock;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
@@ -10,6 +11,9 @@ import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.location.Location;
 import android.location.LocationListener;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Chronometer;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -17,6 +21,10 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.maps.android.SphericalUtil;
+
+import java.util.Timer;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener{
 
@@ -24,6 +32,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private LocationListener locationListener;
     private LocationManager locationManager;
     private boolean readyForMaps;
+    private PolylineOptions rectOptions;
+    private Chronometer stopwatch;
+    private boolean sessionRunning = false;
+    private boolean resetButton = true;
+    private long timeWhenStopped = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +51,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},10);
         }
+        rectOptions = new PolylineOptions();
     }
 
 
@@ -76,12 +90,19 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onPause() {
         super.onPause();
         stopLocationServices();
+        enableButton(false);
+        sessionRunning = false;
     }
 
     @Override
     public void onResume() {
         super.onResume();
         startLocationServices();
+        stopwatch = (Chronometer) findViewById(R.id.stopwatch);
+        stopwatch.setBase(SystemClock.elapsedRealtime());
+        timeWhenStopped = 0;
+        stopwatch.setVisibility(View.INVISIBLE);
+        resetButton = true;
     }
 
     private void startLocationServices() {
@@ -98,8 +119,52 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
     }
 
+    public void startSession(View view){
+        stopwatch = (Chronometer) findViewById(R.id.stopwatch);
+        if(!sessionRunning){
+            stopwatch.setVisibility(View.VISIBLE);
+            stopwatch.setBase(SystemClock.elapsedRealtime() + timeWhenStopped);
+            stopwatch.start();
+            sessionRunning = true;
+            toggleButton(true);
+        }
+        else{
+            timeWhenStopped = stopwatch.getBase() - SystemClock.elapsedRealtime();
+            stopwatch.stop();
+            sessionRunning = false;
+            toggleButton(false);
+        }
+    }
+
+    private void toggleButton(boolean running){
+        Button startStop = (Button) findViewById(R.id.startstop);
+        if(running){
+            startStop.setText("Stop Session");
+            startStop.setBackgroundColor(getResources().getColor(R.color.stop));
+        }
+        else{
+            startStop.setText("Start Session");
+            startStop.setBackgroundColor(getResources().getColor(R.color.start));
+        }
+    }
+
+    private void enableButton(boolean enabled) {
+        Button startStop = (Button) findViewById(R.id.startstop);
+        startStop.setEnabled(enabled);
+        if(!enabled) {
+            startStop.setBackgroundColor(getResources().getColor(R.color.disabled_gray));
+            startStop.setText("Getting location...");
+        }
+        else{
+            startStop.setBackgroundColor(getResources().getColor(R.color.start));
+            startStop.setText("Start Session");
+        }
+    }
+
+
     private class MyLocationListener implements LocationListener {
         boolean centered = false;
+        LatLng lastRecorded;
         @Override
         public void onLocationChanged(Location loc) {
             double longitude = loc.getLongitude();
@@ -108,7 +173,21 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             mMap.animateCamera(CameraUpdateFactory.newLatLng(current));
             if(!centered) {
                 mMap.moveCamera(CameraUpdateFactory.zoomTo(15));
+                lastRecorded = current;
+                rectOptions.add(current);
+                mMap.addPolyline(rectOptions);
                 centered = true;
+            }
+            if(resetButton){
+                enableButton(true);
+                resetButton = false;
+            }
+            double distance = SphericalUtil.computeDistanceBetween(lastRecorded,current);
+            System.out.println(distance);
+            if(distance > 15){
+                lastRecorded = current;
+                rectOptions.add(current);
+                mMap.addPolyline(rectOptions);
             }
         }
         @Override
